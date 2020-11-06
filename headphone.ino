7b58b64245372a15af23b9c61133f21e44a5e3c1
Darkseid49 <65637407+Darkseid49@users.noreply.github.com>
2020-11-06 16:14:25 -0500

    Update headphone.ino
    añadidos comentarios

--- headphone.ino
+++ headphone.ino
@@ -1,13 +1,16 @@
-int microphone = A3
+int microfono=0;
 int sonido=0;
-
 void setup()
+
 {
+   pinMode(6,OUTPUT); // pin de salida A6
+   pinMode(0,INPUT); // pin de entrada A0
 Serial.begin(9600);
+  
 }
 
 void loop()
 {
-sonido = analogRead(microphone);
-Serial.printIn(val);
+sonido = analogRead(microfono); //señal que entra por el micrófono
+analogWrite(microfono, sonido / 4); // señal de salida, la entrada está entre 0 y 1023 la salida puede llegar hasta 255 por eso divido para 4
 }
